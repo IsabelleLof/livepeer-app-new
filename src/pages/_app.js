b6b33a506945eb281ea15bc15e3b982ba3928ad2
IsabelleLof <104595550+IsabelleLof@users.noreply.github.com>
2023-04-26 13:25:54 +0200

    livepeer app, next.js, pratice, connected to livepeer

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,19 @@
+import { createReactClient, studioProvider, LivepeerConfig } from '@livepeer/react';
+
 import '@/styles/globals.css'
 
+const client = createReactClient({
+  provider: studioProvider({ apiKey: '620de107-0ad4-4410-812b-6d6beb912002' }),
+});
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return (
+
+  <LivepeerConfig client={client}>
+  
+  <Component {...pageProps} />
+
+  </LivepeerConfig>
+  );
 }
